4ac1a1ce05f12594e96be94171d26d7d4d554d0a
MaciekZ23 <maciekz232002@o2.pl>
2024-12-21 20:11:22 +0100

    Komponent z polem tekstowym

---
+++ Hooki/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FocusInput from './components/FocusInput';
+
+function App()
+{
+    return (
+        <div>
+            <h1>Komponenty z Hookami</h1>
+            <FocusInput />
+        </div>
+    );
+}
+
+export default App;
