6e4db50e41f327e022a72e6d4609a1b0911758fc
MaciekZ23 <maciekz232002@o2.pl>
2024-12-21 19:19:28 +0100

    Hello World

---
+++ hello_world/src/App.js
@@ -0,0 +1,7 @@
+export default function App()
+{
+    const appDiv = document.createElement('div');
+    appDiv.textContent = 'Hello World!';
+    appDiv.className = 'hello-world';
+    return appDiv;
+}
